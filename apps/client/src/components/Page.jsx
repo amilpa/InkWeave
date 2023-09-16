e87894da3a92c8ae6693ad7d83a0749427857247
aswin-madhav <83914989+aswin-madhav@users.noreply.github.com>
2023-09-16 19:53:07 +0530

    page and wa changed

--- apps/client/src/components/Page.jsx
+++ apps/client/src/components/Page.jsx
@@ -1,8 +1,15 @@
 import React, { useState } from "react";
+import Editor from "../routes/Editor";
+import { AiOutlinePlus } from "react-icons/ai";
 
-export default function Page({ txt, children }) {
+export default function Page({ txt ,newPage,setNewPage}) { 
+  //default value
+  const defaultContent = {
+    type: "doc",
+    content: [],
+  };
+
   const [isHovering, setIsHovering] = useState(false);
-
   const handleMouseOver = () => {
     setIsHovering(true);
   };
@@ -16,21 +23,42 @@ export default function Page({ txt, children }) {
     //creates an <editor> component within the page and
     //adds to current page
   }
+  function addPage() {
+    if(!newPage)
+    setNewPage(true);
+    setPgCount(pgCount + 1);
+  }
+
   function deletePage() {}
   return (
-    <div
-      className="relative min-h-[500px] mx-auto mb-8 p-6 w-full max-w-screen-lg bg-gray-800 border-stone-200 sm:rounded-lg sm:border sm:shadow-lg"
-      onMouseOver={handleMouseOver}
-      onMouseOut={handleMouseOut}
-    >
-      {/* {isHovering && (
+    <>
+      <div
+        className="relative min-h-[500px] mx-auto mb-8 p-6 w-full max-w-screen-lg bg-gray-800 border-stone-200 sm:rounded-lg sm:border sm:shadow-lg"
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        {/* {isHovering && (
         <div>
           <button onClick={seeDetails}></button>
           <button onClick={editPage}></button>
         </div>
       )} */}
-      <div className="text-lg">{txt}</div>
-      {children}
-    </div>
+        <div className="text-lg">{txt}</div>
+        <button
+          onClick={addPage}
+          type="button"
+          className="absolute bottom-8 right-8 py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
+        >
+          <AiOutlinePlus />
+        </button>
+      </div>
+      {newPage && (
+        <Editor
+          className="relative min-h-[500px] w-full max-w-screen-lg text-white border-stone-200 bg-gray-800 sm:mx-auto sm:mb-8 sm:rounded-lg sm:border sm:shadow-lg"
+          defaultValue={defaultContent}
+          extensions={[]}
+        />
+      )}
+    </>
   );
 }
--- apps/client/src/components/WritingArea.jsx
+++ apps/client/src/components/WritingArea.jsx
@@ -1,54 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Editor } from "novel";
 import Page from "./Page";
-import { useState } from "react";
-
-import { AiOutlinePlus } from "react-icons/ai";
 
 export default function WritingArea() {
-  const [chapters, setChapters] = useState([[1, 1]]);
+  const [newPage, setNewPage] = useState(false);
   useEffect(() => {
     localStorage.clear();
   }, []);
-  const [pgCount, setPgCount] = useState(1);
-  const defaultContent = {
-    type: "doc",
-    content: [],
-  };
 
-  const [pages, setPages] = useState([]);
-
-  function addPage() {
-    setPages((pages) => [
-      ...pages,
-      <Editor
-        className="relative min-h-[500px] w-full max-w-screen-lg text-white border-stone-200 bg-gray-800 sm:mx-auto sm:mb-8 sm:rounded-lg sm:border sm:shadow-lg"
-        defaultValue={defaultContent}
-        extensions={[]}
-      />,
-    ]);
-    setPgCount(pgCount + 1);
-  }
-
-  function savePage() {
-    //get content from <editor> and place it in <page/>
-    //then setPages() to add <page>
-    //then remove the last <editor> page
-  }
   return (
     <div>
       {/* <button onClick={savePage}>| save page</button> */}
       <div className="pt-16">
-        <Page txt="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.">
-          <button
-            onClick={addPage}
-            type="button"
-            className="absolute bottom-8 right-8 py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
-          >
-            <AiOutlinePlus />
-          </button>
-        </Page>
-        {pages}
+        <Page
+          setNewPage={setNewPage}
+          newPage={newPage}
+          txt="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        />
       </div>
     </div>
   );
