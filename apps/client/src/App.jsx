c27512a328b8440530b39e41ea0b466e0c900b6e
aswin-madhav <83914989+aswin-madhav@users.noreply.github.com>
2023-09-16 10:50:06 +0530

    routes created

--- apps/client/src/App.jsx
+++ apps/client/src/App.jsx
@@ -1,10 +1,12 @@
-import { Editor } from "novel";
 
+import Editor from "./routes/Editor";
+import {Routes,Route} from "react-router-dom"
 function App() {
   return (
-    <div>
-      <Editor />;
-    </div>
+    <Routes>
+      <Route to="/editor" element={<Editor/>}/>
+      
+    </Routes>
   );
 }
 
